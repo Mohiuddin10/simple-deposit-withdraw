b6e92e3634d4b4bc5ba8e6e09cb73fc06f1bab19
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-10-03 00:21:22 +0600

    done

--- bank.js
+++ bank.js
@@ -5,22 +5,23 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const depositBalance = parseInt(document.getElementById('deposit-amount').innerText);
     const balanceAmount = parseInt(document.getElementById('balance-amount').innerText);
     
+    let totalDeposit = depositBalance + depositInput;
+    document.getElementById('deposit-amount').innerText = totalDeposit;
 
+    let netBalance = balanceAmount + depositInput;
+    document.getElementById('balance-amount').innerText =netBalance;
+    document.getElementById('deposit-input').value = '';
+});
 
-    // add deposit 
-    let newBalance = depositBalance + depositInput;
-    console.log(newBalance);
-    document.getElementById('deposit-amount').innerText = newBalance;
-    document.getElementById('balance-amount').innerText = newBalance;
-     
-})
 
 document.getElementById('withdraw-btn').addEventListener('click', function(){
-    const withdrawField = parseInt(document.getElementById('withdraw-field').innerText);
     const withdrawInput = parseInt(document.getElementById('withdraw-input').value);
+    const withdrawBalance = parseInt(document.getElementById('withdraw-field').innerText);
     const balanceAmount = parseInt(document.getElementById('balance-amount').innerText);
-    newBalance = balanceAmount - withdrawInput;
-    document.getElementById('balance-amount').innerText = newBalance;
-    let newWithdrawAmount = withdrawField + withdrawInput;
-    document.getElementById('withdraw-field').innerText = newWithdrawAmount;
+
+    let totalWithdraw = withdrawBalance + withdrawInput;
+    document.getElementById('withdraw-field').innerText = totalWithdraw;
+    let netBalance = balanceAmount -withdrawInput;
+    document.getElementById('balance-amount').innerText = netBalance;
+    document.getElementById('withdraw-input').value = '';
 })
